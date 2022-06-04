3e04f0ab1fb5fcaea6d3a1baa7c467f330e37467
Felipe <pipecalgaro@gmail.com>
2022-06-04 17:32:19 -0300

    last commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist')
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas :)</h1>')
-})
+app.use('/checklists', checklistRouter)
 
-app.listen(3000, () => {
+app.listen(3002, () => {
     console.log('servidor iniciado');
 })
